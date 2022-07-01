a76e8758a22d3e395aad2892bbdcf0d57e88b09d
ajuljulian <ajuljulian@gmail.com>
2022-07-01 08:12:48 -0700

    add initial entry point.

---
+++ cmd/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// Run - is going to be responsible for the instantiation
+// and startup of our go application
+func Run() error {
+	fmt.Println("starting up our applciation")
+	return nil
+}
+
+func main() {
+	fmt.Println("Go REST API Course")
+	if err := Run(); err != nil {
+		fmt.Println(err)
+	}
+}
